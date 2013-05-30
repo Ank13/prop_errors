be59b7a8e417b5e689705b089aed3cc5c2459b7e
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-05-29 22:16:45 -0700

    final validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,20 @@
+class DateValidator < ActiveModel::Validator
+  def validate(event)
+    unless event.date > Date.today
+      event.errors[:date] << 'event must take place in the future'
+    end
+  end
+end
+ 
+
 class Event < ActiveRecord::Base
-  validates :date, :presence => true
+  include ActiveModel::Validations
+  validates :date, :presence => true 
   validates :title, :presence => true, :uniqueness =>true
   validates :organizer_email, :presence => true, :format => { :with => /.+@.+\..+/,
     :message => "must have a valid format"}
   validates :organizer_name, :presence => true
+  validates_with DateValidator
 end
+
+
